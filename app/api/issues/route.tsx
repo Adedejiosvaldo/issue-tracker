593d2e8dd3a1fd3502903e80aad67df19b4fc51b
Joseph <adedejiosvaldo@gmail.com>
2024-07-12 13:49:23 +0100

    Feature -> Build an API for creating issues

---
+++ app/api/issues/route.tsx
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+import IssueSchema from "./schema";
+import prisma from "@/prisma/client";
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+
+  const validator = IssueSchema.safeParse(body);
+
+  if (!validator.success) {
+    return NextResponse.json(validator.error.errors, { status: 400 });
+  }
+
+  const issue = await prisma.issue.create({
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+  return NextResponse.json(issue, { status: 201 });
+}
