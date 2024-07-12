5495fd67060e52f1e00541f73a38d4bb8ef81108
Joseph <adedejiosvaldo@gmail.com>
2024-07-12 12:13:56 +0100

    Feature -> Added active style in the nav bar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,10 +1,19 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { FaBugs } from "react-icons/fa6";
 import { GiSpottedBug } from "react-icons/gi";
+
+// for complex classnames
+import classnames from "classnames";
+
 const NavBar = () => {
+  // for actie path
+  const currentPath = usePathname();
+
   const links = [
-    { label: "Dashboard", href: "/dashboard" },
+    { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
 
@@ -17,7 +26,9 @@ const NavBar = () => {
         {links.map((link) => (
           <Link
             key={link.href}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={` hover:text-zinc-800 transition-colors ${
+              link.href === currentPath ? "text-zinc-900" : "text-zinc-500"
+            }`}
             href={link.href}
           >
             {link.label}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div className="">Hello Wolrf</div>;
+  return <div className="">Hello Wolrd</div>;
 }
